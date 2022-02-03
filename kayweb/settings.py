b2d37bf1b12ad58a6bf3c9bf69107f2e769009a1
Hz-Kene <70513168+Hz-Kene@users.noreply.github.com>
2022-02-03 02:01:12 +0100

    Add files via upload

--- kayweb/settings.py
+++ kayweb/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
