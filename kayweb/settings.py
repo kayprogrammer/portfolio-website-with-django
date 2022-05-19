cbf409cba655f27f1c48fca5966801e41cbd4742
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-05-19 08:53:53 +0100

    Update settings.py

--- kayweb/settings.py
+++ kayweb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'klw!ux$z6^b1e@d2#r%1*u@n_%a=f$fin68&0n&&_=tn$p=_=n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
